845d57c566559626d328e367a264729fa7361271
Gifaldy Azka <official.gifaldy@gmail.com>
2021-09-03 06:38:20 +0700

    📝 Add target _blank

--- src/Pages/Links.svelte
+++ src/Pages/Links.svelte
@@ -13,26 +13,26 @@
             <hr />
             <div id="stage-1">
                 <h3 class="horizon">⚓ 湊あくあ | Minato Aqua's Links</h3>
-                <p>YouTube : <a href="https://www.youtube.com/channel/UC1opHUrw8rvnsadT-iGp7Cg">Aqua Ch. 湊あくあ</a></p>
+                <p>YouTube : <a href="https://www.youtube.com/channel/UC1opHUrw8rvnsadT-iGp7Cg" target="_blank">Aqua Ch. 湊あくあ</a></p>
                 <p>Website :
                     <ul>
-                        <li><a href="https://hololive.hololivepro.com/en/talents/minato-aqua/">hololive Official Website (EN)</a></li>
-                        <li><a href="https://hololive.hololivepro.com/talents/minato-aqua/">hololive Official Website (JP)</a></li>
-                        <li><a href="https://en.hololive.tv/portfolio/items/433565">hololive.tv (EN)</a></li>
-                        <li><a href="https://www.hololive.tv/portfolio/items/336383">hololive.tv (JP)</a></li>
+                        <li><a href="https://hololive.hololivepro.com/en/talents/minato-aqua/" target="_blank">hololive Official Website (EN)</a></li>
+                        <li><a href="https://hololive.hololivepro.com/talents/minato-aqua/" target="_blank">hololive Official Website (JP)</a></li>
+                        <li><a href="https://en.hololive.tv/portfolio/items/433565" target="_blank">hololive.tv (EN)</a></li>
+                        <li><a href="https://www.hololive.tv/portfolio/items/336383" target="_blank">hololive.tv (JP)</a></li>
                     </ul>
-                <p>Twitter : <a href="https://twitter.com/minatoaqua">@minatoaqua</a></p>
-                <p>Marshmallow : <a href="https://marshmallow-qa.com/minatoaqua">@minatoaqua</a></p>
-                <p>Spotify : <a href="https://open.spotify.com/artist/6GHr4rGFHGRUuzuCuxJA0f">湊あくあ</a></p>
-                <p>TwitCasting : <a href="https://twitcasting.tv/minatoaqua">@minatoaqua</a></p>
-                <p>bilibili : <a href="https://space.bilibili.com/375504219/">湊-阿库娅Official</a></p>
-                <p>OPENREC.tv : <a href="https://www.openrec.tv/user/minatoaqua">@minatoaqua</a></p>
-                <p>Discord Fan Server : <a href="https://discord.gg/akukinhq">discord.gg/akukinhq</a></p>
+                <p>Twitter : <a href="https://twitter.com/minatoaqua" target="_blank">@minatoaqua</a></p>
+                <p>Marshmallow : <a href="https://marshmallow-qa.com/minatoaqua" target="_blank">@minatoaqua</a></p>
+                <p>Spotify : <a href="https://open.spotify.com/artist/6GHr4rGFHGRUuzuCuxJA0f" target="_blank">湊あくあ</a></p>
+                <p>TwitCasting : <a href="https://twitcasting.tv/minatoaqua" target="_blank">@minatoaqua</a></p>
+                <p>bilibili : <a href="https://space.bilibili.com/375504219/" target="_blank">湊-阿库娅Official</a></p>
+                <p>OPENREC.tv : <a href="https://www.openrec.tv/user/minatoaqua" target="_blank">@minatoaqua</a></p>
+                <p>Discord Fan Server : <a href="https://discord.gg/akukinhq" target="_blank">discord.gg/akukinhq</a></p>
             </div>
             <div id="website-related">
                 <h3 class="horizon"><i class="fas fa-globe"></i> Website Related Links</h3>
-                <p><i class="fab fa-github"></i> GitHub Repository : <a href="https://github.com/gifaldyazkaa/aqua-fanpage">@gifaldyazkaa/aqua-fanpage</a></p>
-                <p>This website is Built with <a href="https://svelte.dev">Svelte</a>, <a href="https://sveltestrap.js.org">sveltestrap</a>, <a href="https://www.npmjs.com/package/page">Page.js</a>, and <a href="https://www.npmjs.com/package/svelte-page-progress">svelte-page-progress</a>.</p>
+                <p><i class="fab fa-github"></i> GitHub Repository : <a href="https://github.com/gifaldyazkaa/aqua-fanpage" target="_blank">@gifaldyazkaa/aqua-fanpage</a></p>
+                <p>This website is Built with <a href="https://svelte.dev" target="_blank">Svelte</a>, <a href="https://sveltestrap.js.org" target="_blank">sveltestrap</a>, <a href="https://www.npmjs.com/package/page" target="_blank">Page.js</a>, and <a href="https://www.npmjs.com/package/svelte-page-progress" target="_blank">svelte-page-progress</a>.</p>
             </div>
         </Col>
     </Row>
