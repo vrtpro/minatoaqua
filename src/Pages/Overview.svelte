6041a9a42e1bd787072b746388c76286e4302c11
Gifaldy Azka <official.gifaldy@gmail.com>
2021-09-21 19:41:09 +0700

    style: update css style rule
    Update CSS Style Rule for a (link) tag

--- src/Pages/Overview.svelte
+++ src/Pages/Overview.svelte
@@ -63,6 +63,12 @@
     div.intro {
         padding-top: 2rem;
     }
+    div.intro a {
+        text-decoration: underline;
+    }
+    div.intro a:hover {
+        text-decoration: none;
+    }
     h1, h2 {
         font-family: 'Lexend', sans-serif;
     }
