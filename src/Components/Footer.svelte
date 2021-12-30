26d1b1b377a875d54b86ac1a0298ff96423b4c5b
gifaldyazkaa <official.gifaldy@gmail.com>
2021-12-30 09:56:15 +0700

    chore: update footer

--- src/Components/Footer.svelte
+++ src/Components/Footer.svelte
@@ -1,21 +1,24 @@
 <script lang="ts">
-    
 </script>
 
 <hr />
 <footer>
-    <p class="text-center" id="footer-text">Created by <a href="https://github.com/gifaldyazkaa">Gifaldy Azka</a><br>With <a href="https://svelte.dev">Svelte JS</a> and <a href="https://sveltestrap.js.org/">sveltestrap</a></p>
+  <p class="text-center" id="footer-text">
+    Created by{' '}<a href="https://github.com/gifaldyazkaa">Gifaldy Azka</a
+    >{' '}with
+    {' '}<a href="https://svelte.dev">Svelte</a>
+  </p>
 </footer>
 
 <style>
-    footer #footer-text {
-        font-family: 'Lexend', sans-serif;
-        padding-bottom: 1rem;
-    }
-    footer #footer-text a {
-        text-decoration: none;
-    }
-    footer #footer-text a:hover {
-        text-decoration: underline;
-    }
+  footer #footer-text {
+    font-family: 'Lexend', sans-serif;
+    padding-bottom: 1rem;
+  }
+  footer #footer-text a {
+    text-decoration: none;
+  }
+  footer #footer-text a:hover {
+    text-decoration: underline;
+  }
 </style>
