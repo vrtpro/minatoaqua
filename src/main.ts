0c169593b339693800ec66c1d78f6e9436b9b3ad
Gifaldy Azka <me@falcxxdev.ml>
2022-06-29 16:04:46 +0700

    style: code formatting

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,33 @@
 import App from './App.svelte';
 
 const app = new App({
-  target: document.body,
-  props: {
-    name: {
-      en: 'Minato Aqua',
-      jp: '湊あくあ',
-      cn: '湊-阿库娅',
+    target: document.body,
+    props: {
+        name: {
+            en: 'Minato Aqua',
+            jp: '湊あくあ',
+            cn: '湊-阿库娅',
+        },
+        fanName: {
+            en: 'Aqua Crew',
+            jp: 'あくあクルー',
+        },
+        inRealBirthdate: 'December 01',
+        firstDebut: 'August 8, 2018',
+        generation: 'hololive 2nd Gen',
+        height: '148cm',
+        weight: '4x.x kg',
+        oshiMark: '⚓',
+        introductionWords: 'KonAqua! Minato Aqua desu!',
+        greetingsWord: {
+            en: 'KonAqua!',
+            jp: 'こんあくあ～',
+        },
+        hobby: ['Gaming', 'Singing'],
+        likeThings: ['Anime', 'Manga', 'Omurice'],
+        goals: 'To be the number one idol in the galaxy!',
+        uniqueSkill: 'Able to identify shampoo brand just by smells',
     },
-    fanName: {
-      en: 'Aqua Crew',
-      jp: 'あくあクルー',
-    },
-    inRealBirthdate: 'December 01',
-    firstDebut: 'August 8, 2018',
-    generation: 'hololive 2nd Gen',
-    height: '148cm',
-    weight: '4x.x kg',
-    oshiMark: '⚓',
-    introductionWords: 'KonAqua! Minato Aqua desu!',
-    greetingsWord: {
-      en: 'KonAqua!',
-      jp: 'こんあくあ～',
-    },
-    hobby: ['Gaming', 'Singing'],
-    likeThings: ['Anime', 'Manga', 'Omurice'],
-    goals: 'To be the number one idol in the galaxy!',
-    uniqueSkill: 'Able to identify shampoo brand just by smells',
-  },
 });
 
 export default app;
