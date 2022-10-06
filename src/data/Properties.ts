1a9a0a75ddeb5d96b5cd75fb5fa710edb8fb3443
Gifaldy Azka <me@falcxxdev.ml>
2022-10-06 18:22:23 +0700

    refactor: move props data to dedicated place

---
+++ src/data/Properties.ts
@@ -0,0 +1,26 @@
+export const Prop = {
+    name: {
+        en: 'Minato Aqua',
+        jp: '湊あくあ',
+        cn: '湊-阿库娅',
+    },
+    fanName: {
+        en: 'Aqua Crew',
+        jp: 'あくあクルー',
+    },
+    inRealBirthdate: 'December 01',
+    firstDebut: 'August 8, 2018',
+    generation: 'hololive 2nd Gen',
+    height: '148cm',
+    weight: '4x.x kg',
+    oshiMark: '⚓',
+    introductionWords: 'KonAqua! Minato Aqua desu!',
+    greetingsWord: {
+        en: 'KonAqua!',
+        jp: 'こんあくあ～',
+    },
+    hobby: ['Gaming', 'Singing'],
+    likeThings: ['Anime', 'Manga', 'Omurice'],
+    goals: 'To be the number one idol in the galaxy!',
+    uniqueSkill: 'Able to identify shampoo brand just by smells',
+};
