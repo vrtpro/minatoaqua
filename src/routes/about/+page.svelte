9765ec2b3c5040b83d4d27682af283f0ef4e5c07
Gifaldy Azka <falcxxdev@pm.me>
2023-04-03 00:28:42 +0700

    chore: populate about route with real data

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,24 +9,30 @@
 				<h1
 					class="text-4xl text-right font-bold mt-10 bg-gradient-to-r from-[#df98c2] to-[#4c27b3] text-transparent bg-clip-text"
 				>
-					About
+					Quick overview
 				</h1>
 				<p class="text-base my-3 text-right">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, non, consequuntur, earum
-					eligendi labore dolor maiores cupiditate numquam maxime nesciunt repellendus. Reprehenderit nam
-					accusantium eos eius, corrupti ab cumque consectetur itaque eligendi, voluptatum esse ut minima
-					exercitationem rem nemo ipsa.
+					<span class="font-bold">Minato Aqua</span> (湊あくあ) is a female Japanese Virtual YouTuber
+					associated with hololive. Debutting as part of it's second generations.
+					<br />
+					She is a virtual maid who wears a marine-themed maid uniform and a white mob cap decorated with an anchor
+					though. It has also been described as a "tissue box".
 				</p>
 				<h1
 					class="text-4xl text-right font-bold mt-10 bg-gradient-to-r from-[#df98c2] to-[#4c27b3] text-transparent bg-clip-text"
 				>
-					Something
+					Personality
 				</h1>
 				<p class="text-base my-3 text-right">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, non, consequuntur, earum
-					eligendi labore dolor maiores cupiditate numquam maxime nesciunt repellendus. Reprehenderit nam
-					accusantium eos eius, corrupti ab cumque consectetur itaque eligendi, voluptatum esse ut minima
-					exercitationem rem nemo ipsa.
+					Aqua is a bumbling airhead who somehow manages to become the victim of every joke in which she gets
+					involved. She is also child-ish and prone to throwing tantrums, smashing her keyboard, and
+					hyperventilating when things don't go as planned (which is most of the time). She enjoys playing
+					pranks and causing mischief, but it almost always backfires because of a combination of poor
+					planning and bad luck.
+					<br />
+					<br />
+					According to other Hololive members, Aqua is kind and responsible but extremely introverted while off-camera,
+					only opening up while streaming.
 				</p>
 			</div>
 		</div>
@@ -35,25 +41,36 @@
 				<h1
 					class="text-4xl text-left font-bold mt-10 bg-gradient-to-r from-[#df98c2] to-[#4c27b3] text-transparent bg-clip-text"
 				>
-					Suggestions
+					Likes
 				</h1>
 				<p class="text-base my-3 text-left">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, non, consequuntur, earum
-					eligendi labore dolor maiores cupiditate numquam maxime nesciunt repellendus. Reprehenderit nam
-					accusantium eos eius, corrupti ab cumque consectetur itaque eligendi, voluptatum esse ut minima
-					exercitationem rem nemo ipsa.
+					She likes anime, manga, omurice, singing, and playing games.
+					<br />
+					Often doing gameplay stream and karaoke on YouTube. Her most-played games is Minecraft, HoloCure, APEX
+					Legends, Resident Evil, any many more!
+					<br />
+					She is also doing a member-only exclusive stream on YouTube.
 				</p>
 				<h1
 					class="text-4xl text-left font-bold mt-10 bg-gradient-to-r from-[#df98c2] to-[#4c27b3] text-transparent bg-clip-text"
 				>
-					Nothing
+					Trivias
 				</h1>
-				<p class="text-base my-3 text-left">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, non, consequuntur, earum
-					eligendi labore dolor maiores cupiditate numquam maxime nesciunt repellendus. Reprehenderit nam
-					accusantium eos eius, corrupti ab cumque consectetur itaque eligendi, voluptatum esse ut minima
-					exercitationem rem nemo ipsa.
-				</p>
+				<p class="text-base my-3 text-left" />
+				<ul class="text-base my-3 text-left list-disc px-4">
+					<li>
+						Whenever she puts all focus on gameplay, she often makes unintelligible battle cries such as
+						"nya", "rua", and "wryy."
+					</li>
+					<li>
+						She is also called with "Sololive". The nickname comes from the fact that she tends to play
+						games alone.
+					</li>
+					<li>
+						She uses a lot of exclamation marks in her tweets (!!!). This behavior has spread to other
+						Virtual YouTubers
+					</li>
+				</ul>
 			</div>
 		</div>
 	</FullScreenCard>
