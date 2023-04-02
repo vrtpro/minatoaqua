6cf9c445ef59f52084e66aa00edf3e25c037dd4b
Gifaldy Azka <falcxxdev@pm.me>
2023-04-02 23:01:31 +0700

    chore: repo cleanup (reset)

--- src/global.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="svelte" />
-declare module 'page';
-declare module 'svelte-page-progress';
-declare module 'sveltestrap';
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import App from './App.svelte';
-import { Prop } from './data/Properties';
-
-const app = new App({
-    target: document.body,
-    props: Prop,
-});
-
-export default app;
